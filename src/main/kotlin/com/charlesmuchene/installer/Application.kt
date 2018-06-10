3153ab57df9c877e51b342ad2c39418e98c30dbe
Charles Muchene <charlesmuchene@gmail.com>
2018-06-10 23:24:11 +0300

    Converts to object class

--- src/main/kotlin/com/charlesmuchene/installer/Application.kt
+++ src/main/kotlin/com/charlesmuchene/installer/Application.kt
@@ -2,55 +2,53 @@ package com.charlesmuchene.installer
 
 import com.charlesmuchene.installer.models.SystemAction
 import com.charlesmuchene.installer.ui.HomeScreen
+import com.charlesmuchene.installer.utils.loadConfiguration
+import java.util.*
 import javax.swing.UIManager
 
 /**
  * Application class
  */
-class Application {
+object Application {
 
-    companion object {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val runner = Runner()
+        val output: String? = runInitializationSequence(runner)
+        showUI(runner, output)
+    }
 
-        @JvmStatic
-        fun main(args: Array<String>) {
-            val runner = Runner()
-            val output: String? = runInitializationSequence(runner)
-            showUI(runner, output)
+    /**
+     * Show UI
+     *
+     * @param runner [Runner] instance
+     * @param output System actions output
+     */
+    private fun showUI(runner: Runner, output: String? = null) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
 
-        /**
-         * Show UI
-         *
-         * @param runner [Runner] instance
-         * @param output System actions output
-         */
-        private fun showUI(runner: Runner, output: String? = null) {
-            try {
-                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
+        val homeScreen = HomeScreen(runner)
+        output?.let(homeScreen::addOutput)
+    }
 
-            val homeScreen = HomeScreen(runner)
-            output?.let(homeScreen::addOutput)
+    /**
+     * Run initialization sequence
+     *
+     * @param runner [Runner] instance
+     * @return Output
+     */
+    private fun runInitializationSequence(runner: Runner): String? {
+        var output: String? = null
+        runner.run {
+            output = runSystemAction(SystemAction.InitializeInstaller)
+            output += runSystemAction(SystemAction.InitializeAutomator)
+            output += runSystemAction(SystemAction.InstallApplication)
         }
-
-        /**
-         * Run initialization sequence
-         *
-         * @param runner [Runner] instance
-         * @return Output
-         */
-        private fun runInitializationSequence(runner: Runner): String? {
-            var output: String? = null
-            runner.run {
-                output = runSystemAction(SystemAction.InitializeInstaller)
-                output += runSystemAction(SystemAction.InitializeAutomator)
-                output += runSystemAction(SystemAction.InstallApplication)
-            }
-            return output
-        }
-
+        return output
     }
 
 }
