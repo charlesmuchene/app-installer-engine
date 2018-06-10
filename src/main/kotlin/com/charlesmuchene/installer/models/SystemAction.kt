4bab960b39a737f2a0db49b4ecf8f958e16c0910
Charles Muchene <charlesmuchene@gmail.com>
2018-06-10 23:27:19 +0300

    Applies correct checker name reference

--- src/main/kotlin/com/charlesmuchene/installer/models/SystemAction.kt
+++ src/main/kotlin/com/charlesmuchene/installer/models/SystemAction.kt
@@ -13,7 +13,7 @@ enum class SystemAction {
      * @return Action commands
      */
     fun getCommands(): Pair<Array<String>, Array<String>> = when (this) {
-        InitializeInstaller -> Pair(Commands.pushInstaller, Commands.installInstaller)
+        InitializeInstaller -> Pair(Commands.pushChecker, Commands.installChecker)
         InitializeAutomator -> Pair(Commands.pushAutomator, Commands.installAutomator)
         InstallApplication -> Pair(Commands.pushSBDriverApp, Commands.installSBDriverApp)
     }
