59f7cf03c7cfa0b87c9968c642147a2224f8a3a0
Charles Muchene <charlesmuchene@gmail.com>
2018-06-10 20:07:00 +0300

    Adds comment to constants file

--- src/main/kotlin/com/charlesmuchene/installer/utils/Constants.kt
+++ src/main/kotlin/com/charlesmuchene/installer/utils/Constants.kt
@@ -2,6 +2,9 @@
 
 package com.charlesmuchene.installer.utils
 
+/**
+ * Add any new arguments to the Constants file in the Installer Automator app
+ */
 const val NETWORK_SSID = "network_ssid"
 const val ACCOUNT_EMAIL = "account_email"
 const val NETWORK_PASSWORD = "network_password"
