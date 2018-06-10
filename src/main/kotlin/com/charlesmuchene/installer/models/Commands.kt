a2efcf4a335495cdf44bdd65a1a70a6cda9db4ae
Charles Muchene <charlesmuchene@gmail.com>
2018-06-10 23:25:31 +0300

    Refactors automator commands parsing

--- src/main/kotlin/com/charlesmuchene/installer/models/Commands.kt
+++ src/main/kotlin/com/charlesmuchene/installer/models/Commands.kt
@@ -1,34 +1,46 @@
 package com.charlesmuchene.installer.models
 
+import com.charlesmuchene.installer.utils.*
+import java.util.*
+
 /**
  * Installer/Automator commands
  */
 object Commands {
 
+    private val properties: Properties = loadConfiguration()
+
     private const val androidJunitRunner = "com.charlesmuchene.installer.test/android.support.test.runner.AndroidJUnitRunner"
     private val automatorRunner = arrayOf("adb", "shell", "am", "instrument", "-w", "-r", "-e", "class")
 
-    val resetDeviceBridge = (automatorRunner + createAutomatorCommand("reset")) + androidJunitRunner
+    val resetDeviceBridge = automatorRunner
+            .plus(createAutomatorCommand("reset"))
+            .plus(androidJunitRunner)
 
-    val connectToWifi = (automatorRunner + createAutomatorCommand("connectToWifi")) + androidJunitRunner
+    val connectToWifi = automatorRunner
+            .plus(createAutomatorCommand("connectToWifi"))
+            .plus(Argument.NETWORK_PASSWORD.getArguments(properties))
+            .plus(Argument.NETWORK_SSID.getArguments(properties))
+            .plus(androidJunitRunner)
 
-    val addGoogleAccount = (automatorRunner + createAutomatorCommand("addGoogleAccount")) + androidJunitRunner
+    val addGoogleAccount = automatorRunner
+            .plus(createAutomatorCommand("addGoogleAccount"))
+            .plus(Argument.ACCOUNT_PASSWORD.getArguments(properties))
+            .plus(Argument.ACCOUNT_EMAIL.getArguments(properties))
+            .plus(androidJunitRunner)
 
-    // TODO Determine the path of Installer app
-
-    val pushInstaller = arrayOf("adb", "push", "path_to_debug.apk", "/data/local/tmp/com.charlesmuchene.installer")
-    val installInstaller = arrayOf("adb", "shell", "pm", "install", "-t", "-r",
+    val pushChecker = arrayOf("adb", "push", properties.getProperty(CHECKER_PATH_KEY),
+            "/data/local/tmp/com.charlesmuchene.installer")
+    val installChecker = arrayOf("adb", "shell", "pm", "install", "-t", "-r",
             "\"/data/local/tmp/com.charlesmuchene.installer\"")
 
-    // TODO Determine the path of the Automator app
-
-    val pushAutomator = arrayOf("adb", "push", "path_to_test.apk", "/data/local/tmp/com.charlesmuchene.installer.test")
+    val pushAutomator = arrayOf("adb", "push", properties.getProperty(AUTOMATOR_PATH_KEY),
+            "/data/local/tmp/com.charlesmuchene.installer.test")
     val installAutomator = arrayOf("adb", "shell", "pm", "install", "-t", "-r",
             "\"/data/local/tmp/com.charlesmuchene.installer.test\"")
 
-    // TODO Determine the path of the SB Driver app
-
-    val pushSBDriverApp = arrayOf("adb", "push", "path_to_sb_driver_app", "/data/local/tmp/com.safeboda.driver")
+    val pushSBDriverApp = arrayOf("adb", "push", properties.getProperty(SB_DRIVER_PATH_KEY),
+            "/data/local/tmp/com.safeboda.driver")
     val installSBDriverApp = arrayOf("adb", "shell", "pm", "install", "-t", "-r", "-g",
             "\"/data/local/tmp/com.safeboda.driver\"")
 
