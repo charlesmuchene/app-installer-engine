0873edf2ffb488875b13b5688dc3800ca2401733
Charles Muchene <charlesmuchene@gmail.com>
2018-06-10 23:24:53 +0300

    Adds functionality to load configuration

--- src/main/kotlin/com/charlesmuchene/installer/utils/Utilities.kt
+++ src/main/kotlin/com/charlesmuchene/installer/utils/Utilities.kt
@@ -1,4 +1,39 @@
+@file:JvmName("Utilities")
+
 package com.charlesmuchene.installer.utils
 
-object Utilities {
+import com.charlesmuchene.installer.models.Commands
+import java.io.FileInputStream
+import java.io.FileNotFoundException
+import java.util.*
+
+/**
+ * Utilities file
+ */
+
+/**
+ * Properties file name
+ */
+const val propertiesFile = "installer.properties"
+
+/**
+ * Load app configuration
+ */
+@Throws(FileNotFoundException::class)
+fun loadConfiguration(): Properties {
+    val stream = FileInputStream(propertiesFile)
+    return Properties().apply { load(stream) }
+}
+
+// TODO Remove test implementation
+fun main(args: Array<String>) {
+    println(Commands.connectToWifi.reduce { a, b -> "$a $b" })
+    println(Commands.resetDeviceBridge.reduce { a, b -> "$a $b" })
+    println(Commands.addGoogleAccount.reduce { a, b -> "$a $b" })
+    println(Commands.pushChecker.reduce { a, b -> "$a $b" })
+    println(Commands.installChecker.reduce { a, b -> "$a $b" })
+    println(Commands.pushAutomator.reduce { a, b -> "$a $b" })
+    println(Commands.installAutomator.reduce { a, b -> "$a $b" })
+    println(Commands.pushSBDriverApp.reduce { a, b -> "$a $b" })
+    println(Commands.installSBDriverApp.reduce { a, b -> "$a $b" })
 }
