dbd64cf269f914c80d87e05e46e5cdbe9cc40054
Charles Muchene <charlesmuchene@gmail.com>
2018-06-12 00:43:13 +0300

    Implements loading drivers list

--- src/main/kotlin/com/charlesmuchene/installer/utils/Utilities.kt
+++ src/main/kotlin/com/charlesmuchene/installer/utils/Utilities.kt
@@ -2,8 +2,7 @@
 
 package com.charlesmuchene.installer.utils
 
-import java.io.FileInputStream
-import java.io.FileNotFoundException
+import java.io.*
 import java.util.*
 
 /**
@@ -13,11 +12,33 @@ const val propertiesFile = "installer.properties"
 
 val lineSeparator: String = System.lineSeparator()
 
+val properties: Properties = loadConfiguration()
+
+val driversList: List<Pair<String, String>> by lazy {
+    val result = mutableListOf<Pair<String, String>>()
+    val file = File(properties.getProperty(DRIVERS_PATH_KEY))
+    val reader = BufferedReader(FileReader(file))
+    var line: String? = reader.readLine()
+    while (line != null) {
+        result.add(parseLine(line))
+        line = reader.readLine()
+    }
+    result
+}
+
 /**
  * Load app configuration
  */
 @Throws(FileNotFoundException::class)
-fun loadConfiguration(): Properties {
+private fun loadConfiguration(): Properties {
     val stream = FileInputStream(propertiesFile)
     return Properties().apply { load(stream) }
+}
+
+/**
+ * Parse line
+ */
+fun parseLine(line: String, separator: Char = ','): Pair<String, String> {
+    val (email, password) = line.split(separator)
+    return Pair(email, password)
 }
