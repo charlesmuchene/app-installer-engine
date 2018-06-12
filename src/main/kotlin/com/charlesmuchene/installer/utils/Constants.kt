9c649f2c8105c206ec8e71cfaf1d8b53bfccddd2
Charles Muchene <charlesmuchene@gmail.com>
2018-06-12 08:08:33 +0300

    Adds adb path key

--- src/main/kotlin/com/charlesmuchene/installer/utils/Constants.kt
+++ src/main/kotlin/com/charlesmuchene/installer/utils/Constants.kt
@@ -5,6 +5,7 @@ package com.charlesmuchene.installer.utils
 /**
  * Automator arguments moved to Argument class
  */
+const val ADB_PATH_KEY = "adb_path"
 const val SB_APP_PATH_KEY = "app_path"
 const val DRIVERS_PATH_KEY ="drivers_path"
 const val CHECKER_PATH_KEY = "checker_path"
